d753feff5f3bdc187a5888094c65285698fae561
xkalleas <xkalleas@gmail.com>
2018-04-09 16:52:20 +0300

    .

--- app/src/main/java/com/example/simpledb/MainActivity.java
+++ app/src/main/java/com/example/simpledb/MainActivity.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,132 +18,6 @@ import java.io.OutputStream;
 
 import static android.database.sqlite.SQLiteDatabase.OPEN_READWRITE;
 import static android.database.sqlite.SQLiteDatabase.openDatabase;
-
-class DataBaseHelper extends SQLiteOpenHelper {
-
-    //The Android's default system path of your application database.
-
-    private SQLiteDatabase myDataBase;
-
-    private final Context myContext;
-
-    /**
-     * Constructor
-     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
-     *
-     * @param context
-     */
-    public DataBaseHelper(Context context) {
-
-        super(context, "chinook.db", null, 1);
-        this.myContext = context;
-    }
-
-    /**
-     * Creates a empty database on the system and rewrites it with your own database.
-     */
-    public void createDataBase() throws IOException {
-
-        boolean dbExist = checkDataBase();
-
-        if (dbExist) {
-            //do nothing - database already exist
-        } else {
-            //By calling this method and empty database will be created into the default system path
-            //of your application so we are gonna be able to overwrite that database with our database.
-            this.getWritableDatabase();
-
-            try {
-                copyDataBase();
-            } catch (IOException e) {
-                throw new Error("Error copying database");
-            }
-        }
-    }
-
-    /**
-     * Check if the database already exist to avoid re-copying the file each time you open the application.
-     *
-     * @return true if it exists, false if it doesn't
-     */
-    private boolean checkDataBase() {
-
-        SQLiteDatabase checkDB = null;
-
-        try {
-            String myPath = myContext.getApplicationInfo().dataDir + "/databases/chinook.db";
-
-            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
-        } catch (SQLiteException e) {
-            //database does't exist yet.
-        }
-
-        if (checkDB != null) {
-            checkDB.close();
-        }
-
-        return checkDB != null ? true : false;
-    }
-
-    /**
-     * Copies your database from your local assets-folder to the just created empty database in the
-     * system folder, from where it can be accessed and handled.
-     * This is done by transfering bytestream.
-     */
-    private void copyDataBase() throws IOException {
-
-        //Open your local db as the input stream
-        InputStream myInput = myContext.getAssets().open("chinook.db");
-
-        // Path to the just created empty db
-        String outFileName = myContext.getApplicationInfo().dataDir + "/databases/chinook.db";
-
-        //Open the empty db as the output stream
-        OutputStream myOutput = new FileOutputStream(outFileName);
-
-        //transfer bytes from the inputfile to the outputfile
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myInput.read(buffer)) > 0) {
-            myOutput.write(buffer, 0, length);
-        }
-
-        //Close the streams
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
-    }
-
-    public void openDataBase() throws SQLException {
-        //Open the database
-        String myPath = myContext.getApplicationInfo().dataDir + "/databases/chinook.db";
-
-        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
-    }
-
-    @Override
-    public synchronized void close() {
-        if (myDataBase != null)
-            myDataBase.close();
-
-        super.close();
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        //
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //
-    }
-
-    // Add your public helper methods to access and get content from the database.
-    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
-    // to you to create adapters for your views.
-
-}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -163,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
         } catch (SQLException sqle) {
             throw sqle;
         }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, myDbHelper.getAllAlbums());
+
+        ListView listView = (ListView) findViewById(R.id.lstAlbums);
+        listView.setAdapter(adapter);
+
     }
 
 }
